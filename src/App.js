b8e81c11d91d1f787263f5643fafb5d862af6f02
josky1917 <zhujie930@outlook.com>
2023-10-08 00:44:49 -0400

    fix build failure

--- src/App.js
+++ src/App.js
@@ -17,10 +17,6 @@ function App({ signOut }) {
         <Heading level={1}>We now have Auth!</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          杨珂是个勺
-        </p>
     </View>
   );
 }
