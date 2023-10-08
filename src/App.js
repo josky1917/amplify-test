f8f77293ec7ace73a221d7e035c27355b4914be6
josky1917 <zhujie930@outlook.com>
2023-10-08 00:23:50 -0400

    Add aws amplify authenticator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
           杨珂是个勺
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
